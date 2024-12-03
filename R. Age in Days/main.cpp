feb4d1cf307f738be01abfb2c959ad4539527993
Your Name <you@example.com>
2024-12-03 21:39:42 +0300

    add 1 problem

--- R. Age in Days/main.cpp
+++ R. Age in Days/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+    int x,years,months,days;
+    cin>>x;
 
+    years = (x/365);
+    months = ((x%365)/ 30);
+    days = ((x%365)% 30);
+    cout << years <<" years"<<"\n" <<months<<" months"<<"\n"<<days<<" days"<< endl;
     return 0;
 }
